ba9c45e13302d7fedc6672c706c5c50dd6bf32a1
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-05-04 16:57:02 -0300

    Changed secret JWT

--- src/main/java/br/com/juwer/springauth/AuthorizationServerConfig.java
+++ src/main/java/br/com/juwer/springauth/AuthorizationServerConfig.java
@@ -1,8 +1,8 @@
 package br.com.juwer.springauth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,8 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import java.util.Arrays;
 
@@ -75,10 +74,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
             .authenticationManager(authenticationManager)
             .userDetailsService(userDetailsService)
             .reuseRefreshTokens(false)
+            .accessTokenConverter(jwtAccessTokenConverter())
             .tokenGranter(tokenGranter(endpoints));
     }
 
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+        var jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("n26247586h4576h28475h60824576hdfxg807hdfg87");
 
+        return jwtAccessTokenConverter;
+    }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
