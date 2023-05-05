4349fb42de0e46dcee171fe04a9ea8268967afb0
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-05-05 16:32:50 -0300

    fixed authorization page login

--- src/main/java/br/com/juwer/springauth/AuthorizationServerConfig.java
+++ src/main/java/br/com/juwer/springauth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package br.com.juwer.springauth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,7 +14,11 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import java.util.Arrays;
 
@@ -29,6 +34,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtKeyStoreProperties jwtKeyStoreProperties;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -75,20 +83,36 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
             .userDetailsService(userDetailsService)
             .reuseRefreshTokens(false)
             .accessTokenConverter(jwtAccessTokenConverter())
+            .approvalStore(approvalStore(endpoints.getTokenStore()))
             .tokenGranter(tokenGranter(endpoints));
+    }
+
+    private ApprovalStore approvalStore(TokenStore tokenStore) {
+        var approvalStore = new TokenApprovalStore();
+        approvalStore.setTokenStore(tokenStore);
+
+        return approvalStore;
     }
 
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter(){
         var jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey("n26247586h4576h28475h60824576hdfxg807hdfg87");
+        var jksResource = new ClassPathResource(jwtKeyStoreProperties.getPath());
+        var keyStorePass = jwtKeyStoreProperties.getKeyStorePass();
+        var keyPairAlias = jwtKeyStoreProperties.getKeyPairAlias();
+
+        var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass.toCharArray());
+        var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+
+        jwtAccessTokenConverter.setKeyPair(keyPair);
 
         return jwtAccessTokenConverter;
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.checkTokenAccess("permitAll()") // não precisa passar senha para verificar token
+        security.checkTokenAccess("permitAll()")
+                .tokenKeyAccess("permitAll()") // libera o /oauth/token_key para verificar chave pública
                 .allowFormAuthenticationForClients();
     }
 
--- src/main/java/br/com/juwer/springauth/JwtKeyStoreProperties.java
+++ src/main/java/br/com/juwer/springauth/JwtKeyStoreProperties.java
@@ -1,2 +1,27 @@
-package br.com.juwer.springauth;public class JwtKeyStoreProperties {
+package br.com.juwer.springauth;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Validated
+@Component
+@ConfigurationProperties("algafood.keystore")
+public class JwtKeyStoreProperties {
+
+    @NotBlank
+    private String path;
+
+    @NotBlank
+    private String keyStorePass;
+
+    @NotBlank
+    private String keyPairAlias;
 }
